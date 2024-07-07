8170a56548423d1a5835574f371f55fcf809c667
Mannvika <45762765+Mannvika@users.noreply.github.com>
2024-07-07 14:45:03 -0400

    Built framework for impute function, finished heat_map function, reworked console UI

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
-
+from sklearn.impute import KNNImputer, SimpleImputer
 encoded_columns = set()
 
 
-def select_column(df):
-    cols = df.columns
+def select_column(df, dtype):
+    if dtype == 'all':
+        cols = df.columns
+    else:
+        cols = df.select_dtypes(include=dtype).columns
+    if len(cols) == 0:
+        print('No columns to perform this operation')
+        return -1
+
     col_map = {}
     for i, col in enumerate(cols):
         col_map[i] = col
@@ -15,7 +24,7 @@ def select_column(df):
         print('Select a column to perform an operation on: ')
 
         for i, col in enumerate(cols):
-            print(f"{i}. {col} ({df[col].dtype})")
+            print(f"{i}. {col}")
 
         selection = input()
 
@@ -31,9 +40,25 @@ def normalize(selected_col, df):
 
 def impute_missing_data(selected_col, df):
     print('Overview of Missing Data: ')
-    print(df[df[selected_col.name].isna()])
 
-    options = ['KNN', 'Delete Rows', 'Most Frequent']
+    if df[df[selected_col.name].isna()].empty:
+        print('No missing data for selected column.')
+        return
+
+    print(df[df[selected_col.name].isna()])
+    options = ['Most Frequent', 'Delete Rows', 'Cancel']
+    option = print_options(options)
+
+    if option == len(options) - 1:
+        return
+    if option == len(options) - 2:
+        df.dropna(subset=[selected_col.name], inplace=True)
+        return
+    if option == 0:
+        imputer = SimpleImputer(strategy='most_frequent')
+
+    df[[selected_col.name]] = imputer.fit_transform(df[[selected_col.name]])
+    print(df[selected_col.name])
 
 
 def encode(selected_col, df):
@@ -60,36 +85,54 @@ def print_options(options):
     return int(selection)
 
 
-def main():
-    pd.set_option('display.max_columns', None)
-    print('Welcome to the CSV Trainer!\n')
+def print_heatmap(df):
+    # Extract numerical columns
+    numerical = df.select_dtypes(include=['number'])
 
+    # Calculate the correlation matrix
+    corr = numerical.corr()
+
+    plt.figure(figsize=(10, 5))
+
+    # Plot the heatmap
+    sns.heatmap(corr, annot=True, cmap='coolwarm')
+    plt.show()
+
+
+def main():
+    # Display all features
+    pd.set_option('display.max_columns', None)
+
+    print('Welcome to the CSV Trainer!\n')
     print('Enter the file path of the CSV you wish to train:')
+
+    # Load tabular data based on filepath
     filepath = input()
     df = pd.read_csv(filepath)
 
+    # Display first few data
     print('\nYour Data: ')
     print(df.head(), '\n')
 
-    while True:
-        selected_col = select_column(df)
+    options = ['Impute Data (Categorical)', 'Encode Data (Categorical)', 'View Heatmap (Numerical)', 'Normalize (Numerical)',
+               'Drop Feature (Both)', 'Complete Feature Engineering', 'Leave']
 
-        if selected_col.dtype == object:
-            categorical_options = ['Impute Missing Data', 'Encode', 'Drop Column', 'Cancel']
-            categorical_functions = [impute_missing_data, encode, drop_column]
-            option = print_options(categorical_options)
-            if option == len(categorical_options) - 1:
-                continue
-            else:
-                categorical_functions[option](selected_col, df)
-        else:
-            numerical_options = ['Normalize', 'Drop Column', 'Cancel']
-            numerical_functions = [normalize, drop_column]
-            option = print_options(numerical_options)
-            if option == len(numerical_options) - 1:
-                continue
-            else:
-                numerical_functions[option](selected_col, df)
+    functions = [impute_missing_data, encode, print_heatmap, normalize, drop_column]
+    while True:
+        option = print_options(options)
+        if option == len(options) - 1:
+            break
+        if option == 0 or option == 1:
+            selected_col = select_column(df, ['object'])
+            functions[option](selected_col, df)
+        if option == 2:
+            functions[option](df)
+        if option == 3:
+            selected_col = select_column(df, 'number')
+            functions[option](selected_col, df)
+        if option == 4:
+            selected_col = select_column(df, 'all')
+            functions[option](selected_col, df)
 
 
 if __name__ == '__main__':
